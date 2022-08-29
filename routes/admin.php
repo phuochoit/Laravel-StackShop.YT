2cfb38108407a1b7a58762ac6c02a4e3a8184d00
Phuochoit <phuocck8@gmail.com>
2022-08-29 21:20:43 +0700

    add package develop

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-
 });
