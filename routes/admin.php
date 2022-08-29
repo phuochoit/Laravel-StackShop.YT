8276aaf1407df6198eb7e3b393e0dc1103648ba9
Phuochoit <phuocck8@gmail.com>
2022-08-29 21:18:33 +0700

    add theme admin

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminDashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+});
